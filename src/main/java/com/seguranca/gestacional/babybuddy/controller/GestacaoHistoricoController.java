98cf1dcd4209d30bf8c5c721647892a81e1ae4bc
Nicolly Martins Palhas <rm93892@estudante.fieb.edu.br>
2025-09-23 11:01:55 -0300

    Atualizado 1

--- src/main/java/com/seguranca/gestacional/babybuddy/controller/GestacaoHistoricoController.java
+++ src/main/java/com/seguranca/gestacional/babybuddy/controller/GestacaoHistoricoController.java
@@ -1,4 +1,36 @@
 package com.seguranca.gestacional.babybuddy.controller;
 
+
+import com.seguranca.gestacional.babybuddy.model.entity.GestacaoHistorico;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+// id, nome, email, senha, nivelAcesso, foto, dataCadastro, statusGestacaoHistorico;
+// Getter (get): Apenas lê o valor do atributo.
+// Setter (set): Apenas modifica o valor do atributo.
+
+@RestController
+@RequestMapping("/api/v1/GestacaoHistorico")
+
 public class GestacaoHistoricoController {
+    List<GestacaoHistorico> GestacaoHistoricoHistorico = new ArrayList<>();
+    @GetMapping
+    public List<GestacaoHistorico> findAll() {
+        GestacaoHistorico u1 = new GestacaoHistorico();
+        u1.setId(1L);
+        u1.setData_registro(LocalDateTime.now());
+        u1.setObservacoes("Muitos Enjoos.");
+
+
+
+        // Adicionando o produto
+
+        return GestacaoHistorico;
+    }
 }
+ 
--- src/main/java/com/seguranca/gestacional/babybuddy/model/entity/GestacaoHistorico.java
+++ src/main/java/com/seguranca/gestacional/babybuddy/model/entity/GestacaoHistorico.java
@@ -1,4 +1,61 @@
 package com.seguranca.gestacional.babybuddy.model.entity;
 
+import java.time.LocalDateTime;
+
 public class GestacaoHistorico {
+
+    private Long id;
+    private LocalDateTime data_registro;
+    private String observacoes;
+    private String peso_materno;
+    private String pressao_arterial;
+    private String semana_gestacional;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getData_registro() {
+        return data_registro;
+    }
+
+    public void setData_registro(LocalDateTime data_registro) {
+        this.data_registro = data_registro;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    public String getPeso_materno() {
+        return peso_materno;
+    }
+
+    public void setPeso_materno(String peso_materno) {
+        this.peso_materno = peso_materno;
+    }
+
+    public String getPressao_arterial() {
+        return pressao_arterial;
+    }
+
+    public void setPressao_arterial(String pressao_arterial) {
+        this.pressao_arterial = pressao_arterial;
+    }
+
+    public String getSemana_gestacional() {
+        return semana_gestacional;
+    }
+
+    public void setSemana_gestacional(String semana_gestacional) {
+        this.semana_gestacional = semana_gestacional;
+    }
 }
