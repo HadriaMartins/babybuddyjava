cbeafdd900dcb031cd5fb8ea6b53f31650e713d8
HadriaMartins <rm93892@estudante.fieb.edu.br>
2026-06-03 11:20:25 -0300

    REMOVI DUPLICATAS

--- src/main/java/com/seguranca/gestacional/babybuddy/model/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.seguranca.gestacional.babybuddy.model.repository;
-
-import com.seguranca.gestacional.babybuddy.model.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
